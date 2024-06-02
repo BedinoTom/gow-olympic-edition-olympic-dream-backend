bbbefb13641456fbe1b4834bc8efa51864596c8c
Tom Bedino <tom.bedino@etu.univ-cotedazur.fr>
2024-06-02 21:59:54 +0200

    add all cors

--- gow/main.py
+++ gow/main.py
@@ -9,9 +9,17 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8080",
+    "https://gow.valoriatechnologia.com/"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 VERSION = "0.0.7"
