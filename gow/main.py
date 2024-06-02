e8099543dcce717fc1fc88c592eab37b6547df17
Tom Bedino <tom.bedino@etu.univ-cotedazur.fr>
2024-06-02 21:51:19 +0200

    add all cors

--- gow/main.py
+++ gow/main.py
@@ -12,12 +12,12 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 
 # Dependency
 def get_db():
