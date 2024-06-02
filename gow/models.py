66de84e17d7f2b77d17a9e8ac46c660ff7bbf167
Tom Bedino <tom.bedino@etu.univ-cotedazur.fr>
2024-06-02 20:09:27 +0200

    move basecode

--- gow/models.py
+++ gow/models.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 
-from .database import Base
+from gow.database import Base
 
 class Record(Base):
     __tablename__ = "records"
