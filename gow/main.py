192fa81d619b001a586eacf032c1586a86015bc7
Tom Bedino <tom.bedino@etu.univ-cotedazur.fr>
2024-06-02 21:53:04 +0200

    add all cors

--- gow/main.py
+++ gow/main.py
@@ -11,10 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"]
 )
 
 VERSION = "0.0.6"
